555e0b3d85209638d4d2da98812ce0e4b8d0fc1b
Martyn Taylor <martyntaylor@martyns-mbp.home>
2020-09-20 21:23:06 +0100

    Add following - table migration and relationship

--- resources/views/friends-list.blade.php
+++ resources/views/friends-list.blade.php
@@ -1,10 +1,10 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 <ul>
-    @foreach (range(1,8) as $index)
+    @foreach (auth()->user()->follows as $user)
         <li class="mb-4">
             <div class="flex items-center text-sm">
-                <img src="https://api.adorable.io/avatars/40/abott@adorable.png" alt="John Doe" class="rounded-full mr-2">
-                John Doe
+                <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="rounded-full mr-2">
+                {{ $user->name }}
             </div>
         </li>
     @endforeach
